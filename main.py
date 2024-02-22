82a6bf1bdc66f1ae3062c257bd701c3d441be389
sshai <sshaikh524@gmail.com>
2024-02-22 09:58:00 -0500

    gemini_api_scrap.py: Was able to subscribe
    Deleted scrap work and moved it into its own project

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 from windows.main_window_pkg import main_window as m_w
 
 
-def run_chat_bot():
+def main() -> None:
     m_w.main_window.mainloop()
 
 
 if __name__ == '__main__':
-    run_chat_bot()
+    main()
 
 
 
