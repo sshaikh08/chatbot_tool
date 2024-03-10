863753fbabc8f7d68ab5f58d4a4379a5cec52020
sharkshake <sshake08@gmail.com>
2024-03-10 11:40:34 -0400

    Made some changes to make the window operate similarly to it's windows counter part

--- GUI/all_windows_settings.py
+++ GUI/all_windows_settings.py
@@ -1,7 +1,7 @@
 from tkinter import Tk
 
-WINDOW_WIDTH = '1000'
-WINDOW_HEIGHT = '750'
+WINDOW_WIDTH = '3000'
+WINDOW_HEIGHT = '2000'
 WINDOW_SIZE = f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}'
 
 # WINDOW_COLOR = 'grey'
@@ -10,9 +10,6 @@ window = Tk()
 window.title('Chat bot tool')
 # window.configure(bg=WINDOW_COLOR)
 
-window.resizable(False, False)
-
-window.update()
 
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
@@ -21,6 +18,10 @@ x = int((screen_width / 2) - (int(WINDOW_WIDTH) / 2))
 y = int((screen_height / 2) - (int(WINDOW_HEIGHT) / 2))
 
 window.geometry(f"{WINDOW_SIZE}+{x}+{y}")
+
+window.update()
+
+window.resizable(False, False)
 
 
 if __name__ == '__main__':
