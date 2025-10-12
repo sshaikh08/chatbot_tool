c0f5f476e995ed374dd4319931699d301e49ee68
sshai <sshaikh524@gmail.com>
2025-10-12 18:19:39 -0400

    Added a comment to gemini_request.py say to try catch the specific model being used

--- API_request/google_gemini/gemini_request.py
+++ API_request/google_gemini/gemini_request.py
@@ -15,7 +15,9 @@ def receive_write_response() -> Path:
         GOOGLE_API_KEY = getenv('GOOGLE_API_KEY')
         genai.configure(api_key=GOOGLE_API_KEY)
 
-        model = 'gemini-pro-latest'
+        model = 'gemini-pro-latest' # This needs a try catch plus addition modification
+                                    # Additional modification should at least include putting available models
+                                    # in a dictionary in the constants folder/file
         client = genai.GenerativeModel(model)
         response = client.generate_content(
             "\n".join(text_strings))  # Code review: specifically the ("\n".join(text_strings)
